c7adbf59f572f4b78d058ee79eed4f0d0b1a4447
Tom Dobson <dobs.tx@gmail.com>
2024-09-03 12:39:13 +0100

    Cast primary and foreign keys to strings and rename date and timestamp fields to adhere to dbt best practices

--- models/staging/stg_pets.sql
+++ models/staging/stg_pets.sql
@@ -1,9 +1,11 @@
 select
-    pet_id,
-    customer_id,
-    pet_created_datetime,
-    dob,
+    -- Cast primary and foreign keys to strings
+    CAST(pet_id AS STRING), -- Primary key
+    CAST(customer_id AS STRING), -- Foreign key
+    -- Rename date and timestamp fields to conform to best practice
+    pet_created_datetime AS pet_created_at,
+    dob AS birth_date,
     gender,
-    converted
+    converted AS converted_date
 from
     challenge.analytics_engineer.tails_default__pet;
--- models/staging/stg_subscriptions.sql
+++ models/staging/stg_subscriptions.sql
@@ -1,13 +1,15 @@
 select
-    subscription_id,
-    pet_id,
-    customer_id,
+    -- Cast primary and foreign keys to strings
+    CAST(subscription_id AS STRING) AS subscription_id, -- Primary key
+    CAST(pet_id AS STRING) AS pet_id, -- Foreign key
+    CAST(customer_id AS STRING) AS customer_id, -- Foreign key
     subscription_type,
     status,
-    start_datetime,
-    end_datetime,
-    created_datetime,
-    last_modified_datetime,
+    -- Rename date and timestamp fields to conform to best practice
+    start_datetime AS start_at,,
+    end_datetime AS end_at,
+    created_datetime AS created_at,
+    last_modified_datetime AS last_modified_at,
     order_days,
     tier,
     scheduled_next_delivery_date,
@@ -17,7 +19,7 @@ select
     suspended,
     trial_days,
     fulfilment_date,
-    scheduled_next_billing_datetime,
+    scheduled_next_billing_datetime AS scheduled_next_billing_at,
     pricing_set_id
 from
     challenge.analytics_engineer.tails_default__subscription;
