e2aac17383a59bd059994957dac2a89e172e0a2f
Tom Dobson <dobs.tx@gmail.com>
2024-09-05 17:59:10 +0100

    Add comments for primary and foreign keys

--- models/marts/dim_pet.sql
+++ models/marts/dim_pet.sql
@@ -1,7 +1,7 @@
 select
     -- Cast primary and foreign keys to STRING for consistency across models
-    CAST(pet_id as STRING) as pet_id,
-    CAST(customer_id as STRING) as customer_id,
+    CAST(pet_id as STRING) as pet_id,            -- Primary key
+    CAST(customer_id as STRING) as customer_id,  -- Foreign key
     pet_gender,
     pet_birth_date,
     pet_created_at
