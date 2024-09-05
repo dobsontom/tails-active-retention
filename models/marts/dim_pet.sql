325d40c16c105ebd11776514bab3978066b2a1b7
Tom Dobson <dobs.tx@gmail.com>
2024-09-05 17:57:05 +0100

    Remove converted_date, reorder fields for rationality

--- models/marts/dim_pet.sql
+++ models/marts/dim_pet.sql
@@ -2,9 +2,8 @@ select
     -- Cast primary and foreign keys to STRING for consistency across models
     CAST(pet_id as STRING) as pet_id,
     CAST(customer_id as STRING) as customer_id,
-    pet_created_at,
-    pet_birth_date,
     pet_gender,
-    converted_date  -- Nullable; will show when pet converted to a subscription
+    pet_birth_date,
+    pet_created_at
 from
     {{ ref('stg_pets') }}
