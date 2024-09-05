727092851bc2b8902b5f1bdb9df64cb4033aa903
Tom Dobson <dobs.tx@gmail.com>
2024-09-05 16:16:16 +0100

    Cast active day and trial end as dates to fix recursion behaviour, update comments, add trial_start_date to final SELECT

--- models/intermediate/int_trial_active_days.sql
+++ models/intermediate/int_trial_active_days.sql
@@ -1,13 +1,13 @@
-
 -- Use recursion to generate a row for every every day between
--- the trials start and trial end date for each subscritiption
+-- the trial start and trial end date for each subscription
 with recursive active_days_cte as (
-    -- Use start_at as the trial start date
+    -- Use start_at as the base
     select
         sub.subscription_id,
         sub.customer_id,
         sub.pet_id,
         sub.start_at as active_day,
+        sub.trial_start_at,
         sub.trial_end_at,
         sub.converted_date
     from
@@ -15,12 +15,14 @@ with recursive active_days_cte as (
 
     union all
 
-    -- Recursive case: generate the next active day
+    -- Add one day to the active day until it equals the trial
+    -- end date, then repeat for the next subscription
     select
         sub.subscription_id,
         sub.customer_id,
         sub.pet_id,
         DATEADD(day, 1, cte.active_day) as active_day,
+        sub.trial_start_at,
         sub.trial_end_at,
         sub.converted_date
     from
@@ -29,7 +31,7 @@ with recursive active_days_cte as (
         {{ ref('int_trial_subscriptions') }} as sub
         on cte.subscription_id = sub.subscription_id
     where
-        cte.active_day <= sub.trial_end_at
+        CAST(cte.active_day as DATE) <= CAST(sub.trial_end_at as DATE)
 )
 -- Select relevant fields for downstream analysis
 select
@@ -37,6 +39,7 @@ select
     customer_id,
     pet_id,
     active_day,
+    trial_start_at,
     trial_end_at,
     converted_date
 from
