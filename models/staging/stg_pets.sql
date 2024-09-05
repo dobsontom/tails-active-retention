c54117274bc99a4d5faf5edfa52440367f7b1d65
Tom Dobson <dobs.tx@gmail.com>
2024-09-05 18:42:24 +0100

    Fix typo, add primary and foreign key comments

--- models/staging/stg_pets.sql
+++ models/staging/stg_pets.sql
@@ -5,9 +5,9 @@ with source as (
 )
 
 select
-    -- Cast primary and foreign keys to STRING for consitency across models
-    CAST(pet_id as STRING) as pet_id,
-    CAST(customer_id as STRING) as customer_id,
+    -- Cast primary and foreign keys to STRING for consistency across models
+    CAST(pet_id as STRING) as pet_id,            -- Primary key
+    CAST(customer_id as STRING) as customer_id,  -- Foreign key
     -- Rename date and timestamp fields for consistency
     pet_created_datetime as pet_created_at,
     dob as pet_birth_date,
