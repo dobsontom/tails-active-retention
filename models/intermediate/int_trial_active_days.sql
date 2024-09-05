ac912062a1f90a8d906dc197fd59a443e6ddf4ce
Tom Dobson <dobs.tx@gmail.com>
2024-09-05 15:41:39 +0100

    Remove flagging to include in another model, remove sorting

--- models/intermediate/int_trial_active_days.sql
+++ models/intermediate/int_trial_active_days.sql
@@ -1,6 +1,6 @@
 
--- Use recursion to generate a row for every every day between the subscription start
--- and subscription end date
+-- Use recursion to generate a row for every every day between
+-- the trials start and trial end date for each subscritiption
 with recursive active_days_cte as (
     -- Use start_at as the trial start date
     select
@@ -22,8 +22,7 @@ with recursive active_days_cte as (
         sub.pet_id,
         DATEADD(day, 1, cte.active_day) as active_day,
         sub.trial_end_at,
-        sub.converted_date,
-        1 as active_trial_flag
+        sub.converted_date
     from
         active_days_cte as cte
     join
@@ -38,17 +37,7 @@ select
     customer_id,
     pet_id,
     active_day,
-    -- Apply an active flag to all days except the converted date
-    case
-        when active_day = converted_date then 0
-        else 1
-    end as active_trial_flag,
-    -- Apply a conversion flag to days where the active day equals the converted date
-    case
-        when active_day = converted_date then 1
-        else 0
-    end as converted_flag
+    trial_end_at,
+    converted_date
 from
     active_days_cte
-order by
-    subscription_id, active_day
